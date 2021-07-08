c21d36296c81a6297c9fc06b4a6752ca907e5b41
Valentin Sene <pro@valentinsene.me>
2021-07-08 22:50:07 +0200

    [+] img.py: Add create gif function

--- src/img.py
+++ src/img.py
@@ -1,15 +1,19 @@
 from PIL import Image, ImageDraw
 
-def createImage(path, cells):
-    img = Image.new('RGB', (1120, 520), color = 'white')
+def create_image(size, cells):
+    img = Image.new("RGB", size, color="white")
     drawer = ImageDraw.Draw(img)
     y = 0
 
     for line in cells:
         x = 0
         for cell in line:
-            color = "#000000" if cell == '1' else "#FFFFFF"
+            color = "#FFFFFF" if cell == 1 else "#000000"
             drawer.rectangle([(x, y), (x + 10, y + 10)], fill=color)
             x += 10
         y += 10
-    img.save(path)
+    return img
+
+def create_gif(path, size, images):
+    dest = Image.new("RGB", size)
+    dest.save(path, save_all=True, append_images=images)
